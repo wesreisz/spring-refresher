63799b8cc31d42075b86633606675d6332c056be
Wesley Reisz <wes@wesleyreisz.com>
2022-07-11 15:05:17 -0400

    initial commit

---
+++ sample-11/src/main/java/com/wesleyreisz/sample11/Sample11Application.java
@@ -0,0 +1,13 @@
+package com.wesleyreisz.sample11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sample11Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sample11Application.class, args);
+    }
+
+}
---
+++ sample-11/src/test/java/com/wesleyreisz/sample11/Sample11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wesleyreisz.sample11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sample11ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
